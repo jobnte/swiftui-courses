e195a1aacd93ee85e73d714b57d8931532423df4
Jorge Bustamante <jorgelub95@gmail.com>
2024-07-12 22:00:04 +0200

    Initial commit

---
+++ HackingWithSwift/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jorge Bustamante on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
