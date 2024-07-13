110c83e8e307345982ee0e077a1b0f443fa1f385
Jorge Bustamante <jorgelub95@gmail.com>
2024-07-13 11:08:37 +0200

    Complete challenge: - Add a header to the third section, saying “Amount per person” - Add another section showing the total amount for the check – i.e., the original amount plus tip value, without dividing by the number of people. - Change the tip percentage picker to show a new screen rather than using a segmented control, and give it a wider range of options – everything from 0% to 100%. Tip: use the range 0..<101 for your range rather than a fixed array.

--- HackingWithSwift/WeSplit/WeSplit/ContentView.swift
+++ HackingWithSwift/WeSplit/WeSplit/ContentView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @FocusState private var amountIsFocused: Bool
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     let tipPercentages = [10, 15, 20, 25, 0]
-    
+
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
@@ -30,6 +31,7 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
 
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -48,8 +50,15 @@ struct ContentView: View {
                     .pickerStyle(.segmented)
                 }
 
-                Section {
+                Section("Amount per person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
                 }
             }
             .navigationTitle("WeSplit")
