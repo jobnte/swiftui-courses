628d7f9d59e270e8a84f3e9486ee2927f30866d8
Jorge Bustamante <jorgelub95@gmail.com>
2024-07-18 17:39:09 +0200

    feat: Enhance State Management and View Modifiers in SwiftUI
    - Implemented @State property wrapper for state management
    - Added toggle and fixedSize viewModifier for dynamic UI updates
    - Demonstrated view re-evaluation upon state changes with conditional logic
    - Integrated state property with TextField for user input management
    - Utilized searchable view modifier within a NavigationStack
    - Applied onChange for responding to state changes dynamically

--- Udemy/LearnState/LearnState/ContentView.swift
+++ Udemy/LearnState/LearnState/ContentView.swift
@@ -9,17 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State private var isOn: Bool = false
+    @State private var search: String = ""
+    @State private var friends: [String] = ["John", "Mary", "Jorge", "Steven", "Jerry"]
+    @State private var filteredFriends: [String] = []
 
     var body: some View {
         VStack {
-            Toggle(isOn: $isOn, label: {
-                Text(isOn ? "ON" : "OFF")
-            }).fixedSize()
+            List(filteredFriends, id: \.self) { friend in
+                Text(friend)
+            }
+            .listStyle(.plain)
+            .searchable(text: $search)
+            .onChange(of: search) { oldValue, newValue in
+                if newValue.isEmpty {
+                    filteredFriends = friends
+                } else {
+                    filteredFriends = friends.filter { $0.contains(search) }
+                }
+            }
+
+            Spacer()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding()
-        .background(isOn ? .yellow : .black)
+        .onAppear {
+            filteredFriends = friends
+        }
+        .navigationTitle("Friends")
     }
 }
 
@@ -27,9 +42,15 @@ struct ContentView: View {
 // Use of toggle and fixedSize viewModifier
 // When STATE changed the SwiftUI view gets re-evaluate
 // Use different conditions to make changes in the view
+// Use of state property for TextField
+
+// Use of Searchable view modifier -- requirement, needs a navigationStack
+// Use onChange
 
 
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
