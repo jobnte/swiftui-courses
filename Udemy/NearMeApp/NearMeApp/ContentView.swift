7726597d7c6d7021d9a22d5711dd57e0385570dc
Jorge Bustamante <jorgelub95@gmail.com>
2024-07-25 12:21:32 +0200

    feat: Implement Search for Nearby Businesses in NearMe App (Section 25)
    - Added Search TextField for user input
    - Retrieved nearby landmarks using MKLocalSearch
    - Implemented Landmark annotation for map display

--- Udemy/NearMeApp/NearMeApp/ContentView.swift
+++ Udemy/NearMeApp/NearMeApp/ContentView.swift
@@ -11,8 +11,47 @@ import MapKit
 struct ContentView: View {
     @ObservedObject private var locationManager = LocationManager()
 
+    @State private var search: String = ""
+    @State private var landmarks = [Landmark]()
+
+    private func getNearByLandmarks() {
+
+        guard let location = self.locationManager.location else {
+            return
+        }
+
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = self.search
+        request.region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+
+            guard let response = response, error == nil else {
+                return
+            }
+
+            let mapItems = response.mapItems
+            self.landmarks = mapItems.map {
+                Landmark(placemark: $0.placemark)
+            }
+
+        }
+
+    }
+
     var body: some View {
-        MapView()
+        ZStack(alignment: .top) {
+            MapView(landmarks: self.landmarks)
+
+            TextField("Search", text: self.$search, onEditingChanged: { _ in }) {
+                self.getNearByLandmarks()
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+            .offset(y: 44)
+
+        }
     }
 }
 
@@ -23,3 +62,7 @@ struct ContentView: View {
 // Create MapView using UIViewRepresentable
 // Create Coordinator class to handle events of MapView
 // Zoom to user location using MKUserLocation
+
+// Add Search TextField
+// Get near by landmarks using MKLocalSearch
+// Implement Landmark annotation
--- Udemy/NearMeApp/NearMeApp/Views/MapView.swift
+++ Udemy/NearMeApp/NearMeApp/Views/MapView.swift
@@ -11,17 +11,27 @@ import MapKit
 
 struct MapView: UIViewRepresentable {
 
+    let landmarks: [Landmark]
+
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
 
     func makeUIView(context: Context) -> MKMapView {
-
         let map = MKMapView()
         map.showsUserLocation = true
         map.delegate = context.coordinator
         return map
     }
 
-    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) { }
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        updateAnnotations(from: uiView)
+    }
+
+    private func updateAnnotations(from mapView: MKMapView) {
+        mapView.removeAnnotations(mapView.annotations)
+        let annotations = self.landmarks.map(LandmarkAnnotation.init)
+        mapView.addAnnotations(annotations)
+
+    }
 }
