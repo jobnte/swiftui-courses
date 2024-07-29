c6216070b30415674c4e808b58ea0c5cc8052561
Jorge Bustamante <jorgelub95@gmail.com>
2024-07-29 22:59:40 +0200

    feat: Implement List, Bundle, and UITextChecker in SwiftUI
    - Introduced List for displaying data in a scrollable list
    - Utilized Bundle to load resources within the app
    - Implemented UITextChecker for text checking and correction

---
+++ HackingWithSwift/WorldScramble/WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Jorge Bustamante on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
